7e8c14c7e156b30de9917e018915efc9dd0b334a
Fr4n <fjsprog@outlook.com>
2026-06-21 16:09:41 -0600

    Added call to MPI_Comm_Size to check that there are at least 2 processes  (at least one client)

--- src/main.c
+++ src/main.c
@@ -3,16 +3,16 @@
 #include "../include/protocol.h"
 
 int main(int argc, char **argv) {
-    int rank;
+    int rank, process_num;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
+    MPI_Comm_size(MPI_COMM_WORLD, &process_num);
     if (rank == COORDINATOR) {
-        run_coordinator();
+        run_coordinator(process_num);
     } else {
         run_client(rank);
     }
-    
+
     MPI_Finalize();
     return 0;
 }
