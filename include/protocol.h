295b713dfb242d6f636121aa521fefc1ddd188d4
Fr4n <fjsprog@outlook.com>
2026-06-21 15:09:33 -0600

    Defined initial protocol for messages (might change later)

---
+++ include/protocol.h
@@ -0,0 +1,34 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+
+#define MAX_NAME_LEN      32
+#define MAX_MSG_LEN      256
+#define MAX_DIFF_LIST      1
+#define MAX_LIST_MEMBERS 16
+#define COORDINATOR 0
+
+
+typedef enum {
+    TAG_REGISTER,
+    TAG_DIRECT,
+    TAG_DIFFUSION,
+    TAG_DISCONNECT,
+    TAG_ERROR
+} msg_type_t;
+
+
+typedef struct {
+    int   sender_rank;
+    char  sender_name[MAX_NAME_LEN];
+ 
+    int   receiver_rank;
+    char  list_name[MAX_NAME_LEN];
+ 
+    int   message_length;
+    char  message_body[MAX_MSG_LEN];   
+ 
+    long  timestamp;
+} Message_t;
+
+#endif
