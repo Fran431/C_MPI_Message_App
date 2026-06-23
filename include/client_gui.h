52defe84f19954530fecbcaaf0a4e0a2dfda09f6
Fr4n <fjsprog@outlook.com>
2026-06-22 20:37:14 -0600

    Added struct with data required by gtk thread

--- include/client_gui.h
+++ include/client_gui.h
@@ -3,10 +3,22 @@
  
 #include <stdio.h>
 #include <mpi.h>
+#include <gtk/gtk.h>
 
 
 #include "protocol.h"
 #include "queue.h"
+
+typedef struct gtk_data{
+    int rank;
+    char name[MAX_NAME_LEN];
+    Queue_t incoming_queue;
+    Queue_t outgoing_queue;
+    GtkTextBuffer* chat_buffer;
+    GtkWidget* chat_view;
+    GtkWidget* receiver_entry;
+    GtkWidget* message_entry;
+} GTK_data_t;
 
 void run_GUI_client(int rank, int num_process);
  
